3d6e9a42279722184de4f394fe00bb6fc697ca73
aicoresynapseai <aicoresynapseai@gmail.com>
2025-07-24 21:31:09 +0530

    Add/update SecureJavaPipelineAI/src/main/java/com/example/securejavapipeline/SecureJavaPipelineApplication.java

---
+++ SecureJavaPipelineAI/src/main/java/com/example/securejavapipeline/SecureJavaPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.securejavapipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureJavaPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureJavaPipelineApplication.class, args);
+    }
+
+}

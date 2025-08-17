f183ede6249daf467de26f5c47a287390b7edd86
aicoresynapseai <aicoresynapseai@gmail.com>
2025-08-17 17:30:55 +0530

    Add/update ShellScriptingWorkshop/01_hello_world.sh

---
+++ ShellScriptingWorkshop/01_hello_world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# ShellScriptingWorkshop/01_hello_world.sh
+# A very basic script to print a greeting.
+
+# The 'echo' command is used to display text on the standard output.
+echo "Hello, Shell Scripting Workshop!"
+echo "This is your first Bash script."
